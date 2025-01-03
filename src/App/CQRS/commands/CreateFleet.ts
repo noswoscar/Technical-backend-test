7b42c6b0ce816dde466989288430c564166f5d0b
noswoscar <noswoscar@gmail.com>
2025-01-03 16:11:19 +0100

    fleetrepository insert in createfleet

--- src/App/CQRS/commands/CreateFleet.ts
+++ src/App/CQRS/commands/CreateFleet.ts
@@ -1,23 +1,28 @@
 import { DIContainer } from '../../DIContainer'
 import { Fleet } from '../../../Domain/entities/Fleet'
 import { FleetIdentity } from '../../../Domain/valueObjects/FleetIdentity'
+import { FleetRepository } from '../../../Infra/Repositories/FleetRepository'
 import ParkingApp from '../../app'
 
 export class CreateFleet {
       execute(fleetIdentity: FleetIdentity): Fleet {
             const app = DIContainer.resolve<ParkingApp>('app')
+            const fleetRepository = new FleetRepository()
             const fleet = new Fleet(fleetIdentity, [])
-            if (
-                  !app
-                        .getFleets()
-                        .find(
-                              (fleetItem) =>
-                                    fleetItem.getFleetId() ===
-                                    fleet.getFleetId()
-                        )
-            ) {
-                  app.getFleets().push(fleet)
-            }
+            fleetRepository.insert(fleet)
             return fleet
+
+            // if (
+            //       !app
+            //             .getFleets()
+            //             .find(
+            //                   (fleetItem) =>
+            //                         fleetItem.getFleetId() ===
+            //                         fleet.getFleetId()
+            //             )
+            // ) {
+            //       app.getFleets().push(fleet)
+            // }
+            // return fleet
       }
 }

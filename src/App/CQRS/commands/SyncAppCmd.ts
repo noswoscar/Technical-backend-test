8e93055bf872e775ab6db23198d2cbb4a372dfee
noswoscar <noswoscar@gmail.com>
2025-01-09 19:06:27 +0100

    rename execute to standard

--- src/App/CQRS/commands/SyncAppCmd.ts
+++ src/App/CQRS/commands/SyncAppCmd.ts
@@ -2,7 +2,7 @@ import { Fleet } from '../../../Domain/agregates/Fleet'
 import { FleetRepository } from '../../../Infra/Repositories/FleetRepository'
 
 export class SyncAppCmd {
-      execute = async (): Promise<Array<Fleet> | undefined> => {
+      async execute(): Promise<Array<Fleet> | undefined> {
             const fleetRepositiory = new FleetRepository()
             const res = await fleetRepositiory.findFleets()
             if (!res) return undefined

2b9af22ee4106401fab1e1dc9dde82ef61f83f01
noswoscar <noswoscar@gmail.com>
2025-01-09 14:57:41 +0100

    use EcmaScript's parameter destructuring with default values to create an optional fleetIdentity parameter

--- src/Domain/valueObjects/FleetIdentity.ts
+++ src/Domain/valueObjects/FleetIdentity.ts
@@ -2,10 +2,12 @@ export class FleetIdentity {
       private id: string
       private userName: string
       private owner: string
-      constructor(userName: string) {
-            this.id =
-                  new Date().getTime().toString(36) +
+      constructor(
+            userName: string,
+            id = new Date().getTime().toString(36) +
                   Math.random().toString(36).slice(2)
+      ) {
+            this.id = id
             this.userName = userName
             this.owner = userName
       }

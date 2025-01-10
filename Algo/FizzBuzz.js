7bcf7ebd35955baaf10d705fcd535dbcf51c9b2f
noswoscar <noswoscar@gmail.com>
2025-01-10 09:32:20 +0100

    Ajout de FizzBuzz dans la partie algo

---
+++ Algo/FizzBuzz.js
@@ -0,0 +1,19 @@
+const Fizzbuzz = (N) => {
+      for (let i = 1; i <= N; i++) {
+            if (i % 3 === 0 && i % 5 === 0) {
+                  console.log('FizzBuzz')
+                  continue
+            }
+            if (i % 3 === 0) {
+                  console.log('Fizz')
+                  continue
+            }
+            if (i % 5 === 0) {
+                  console.log('Buzz')
+                  continue
+            }
+            console.log(i)
+      }
+}
+
+Fizzbuzz(100)
---
+++ Algo/Readme.md
@@ -0,0 +1,8 @@
+# Classical FizzBuzz
+
+Display numbers between 1 and N by following the rules:
+
+- if number can be divided by 3: display Fizz ;
+- if number can be divided by 5: display Buzz ;
+- if number can be divided by 3 AND 5 : display FizzBuzz ;
+- else: display the number.
